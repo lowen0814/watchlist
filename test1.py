696e774d6132eedb1bd75eaa86b3237f9f46f33c
lowen0814 <37492685+lowen0814@users.noreply.github.com>
2019-11-09 17:56:53 +0800

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,20 @@
+def binary_serch(list, item):
+    low = 0
+    high = len(list)-1
+
+    while(low <= high):
+        mid = (low + high)/2
+        guess = list[mid]
+
+        if guess == item:
+            return list[mid]
+        if guess > item:
+            high = mid -1
+        else :
+            low = mid + 1
+
+    return None
+
+my_list = [1,3,5,6,34,67,78,9]
+print binary_serch(my_list,6)
+print binary_serch(my_list,7)
